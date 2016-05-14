197a44c9906120c50a3cba04cffd735315ec0d71
Emre Kayan <emrekayan@gmail.com>
2016-05-14 20:37:49 +0300

    Add service methods

--- app/service/index.js
+++ app/service/index.js
@@ -1,22 +1,27 @@
 const Storage = require('../storage');
 const words = require('../lib/enDictionary');
 
-var Service = function (config) {
+const util = require('util');
+
+var Service = function (config, storage) {
     this.config = config;
-    this.storage = new Storage(config);
+    this.storage = storage;
 };
 
 Service.prototype._generateRandomEmailAddress = function () {
-    var noun = words.nouns[Math.random() * words.nouns.length];
-    var adjective = words.adjectives[Math.random() * words.nouns.length];
+    var noun = words.nouns[Math.floor(Math.random() * words.nouns.length)];
+    var adjective = words.adjectives[Math.floor(Math.random() * words.nouns.length)];
 
     return util.format('%s-%s@%s', noun, adjective, this.config.email.domain);
 };
 
 Service.prototype.generateAddress = function (chatID) {
     var emailAddress = this._generateRandomEmailAddress();
+    return emailAddress;
+};
 
-
+Service.prototype.registerNewUser = function (chatID) {
+    
 };
 
 module.exports = Service;
